602e0f145b5715e74af4b67984be589a7bef57e8
RaulMF40 <144492714+RaulMF40@users.noreply.github.com>
2024-06-27 11:31:33 +0200

    Update file.js
    Cambios realizados para reutilización del storage de cloudinary cambiando la carpeta, que cuando realicemos un post de cualquiera de las rutas (films, productoras, users) en cloudinary se cree una carpeta con image correspondiente, que subamos.

--- src/middlewares/file.js
+++ src/middlewares/file.js
@@ -2,13 +2,23 @@ const multer = require('multer')
 const cloudinary = require('cloudinary').v2
 const { CloudinaryStorage } = require('multer-storage-cloudinary')
 
-const storage = new CloudinaryStorage({
-  cloudinary: cloudinary,
-  params: {
-    folder: 'Films',
-    allowedFormats: ['jpg', 'png', 'jpeg', 'gif', 'webp', 'avif']
-  }
-})
 
-const upload = multer({ storage })
+/* Función para obtener el almacenamiento de Cloudinary con una carpeta dinámica
+ @param {string} folderName - Nombre de la carpeta en Cloudinary
+ @returns {CloudinaryStorage} - Instancia de almacenamiento de Cloudinary
+*/
+
+const getStorage = (folderName) => {
+  return new CloudinaryStorage({
+    cloudinary: cloudinary,
+    params: {
+      folder: folderName,
+      allowedFormats: ['jpg', 'png', 'jpeg', 'gif', 'webp', 'avif']
+    }
+  })
+}
+
+// Middleware para cambiar dinámicamente la carpeta de Cloudinary, en este caso crearemos carpetas de films, users y productoras
+const upload = (folderName) => multer({ storage: getStorage(folderName) })
+
 module.exports = upload
